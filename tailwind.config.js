7498cabc58da1158c290768c879b1df237864c62
William Meneses <jwilliam.anjos@gmail.com>
2022-05-05 22:36:48 -0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          500: "#8257E5",
+        }
+      }
+    },
+  },
+  plugins: [],
+}
